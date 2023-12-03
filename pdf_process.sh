ab2563c8ffb9a8b74ad41a47c47c7f1d4dee4909
Emmanuel Prochasson <eprochasson@gmail.com>
2023-12-03 18:50:25 -0500

    v2

--- pdf_process.sh
+++ pdf_process.sh
@@ -1,4 +1,5 @@
-pdfposter -m a4 slat-curves.pdf -p a1 slat-curves-a4.pdf
-pdfposter -m letter slat-curves.pdf -p a1 slat-curves-letter.pdf
-pdftk slat-curves-letter.pdf cat 1 2 3 4 7 output slat-curves-letter-noblank.pdf
-pdftk slat-curves-a4.pdf cat 1 2 3 4 7 output slat-curves-a4-noblank.pdf
+pdfposter -m a4 drill-template-a1.pdf -p a1 /tmp/template-a4-with-blanks.pdf
+pdftk /tmp/template-a4-with-blanks.pdf cat 1 2 3 6 9 output drill-template-a4.pdf
+pdfposter -m letter drill-template-a1.pdf -p a1 /tmp/template-letter-with-blanks.pdf
+pdftk /tmp/template-letter-with-blanks.pdf cat 1 2 3 6 9 output drill-template-letter.pdf
+
